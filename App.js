a79e078524e4f47ec616402245108887dbe7b4b4
YaroslavLazorenko <lazorenko85@gmail.com>
2023-02-18 18:02:34 +0200

    Add screens with basic markup

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
+  const [screen, setScreen] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>HW2</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {screen === 'Registration' && <RegistrationScreen />}
+      {screen === 'Login' && <LoginScreen />}
+      {screen === '' && (
+        <View style={styles.container}>
+          <Button
+            onPress={() => {
+              setScreen('Registration');
+            }}
+            title="Registration screen"
+          />
+          <Button
+            onPress={() => {
+              setScreen('Login');
+            }}
+            title="Login screen"
+          />
+        </View>
+      )}
+    </>
   );
 }
 
