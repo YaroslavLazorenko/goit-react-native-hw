82abea585b704f0613f69988fd40535b25828af4
YaroslavLazorenko <lazorenko85@gmail.com>
2023-02-27 02:01:42 +0200

    Add header navigation and header styles

--- router.js
+++ router.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -12,11 +13,21 @@ import CreatePostsScreen from './Screens/mainScreen/CreatePostsScreen';
 import ProfileScreen from './Screens/mainScreen/ProfileScreen';
 import CommentsScreen from './Screens/mainScreen/CommentsScreen';
 
+import { AuthContext } from './context';
+
+import SignOutIcon from './assets/images/log-out.svg';
+import GoBackIcon from './assets/images/arrow-left.svg';
+
 const AuthStack = createStackNavigator();
 const MainStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
 const TabRouter = () => {
+  const { setIsAuth } = useContext(AuthContext);
+  const signOut = () => {
+    setIsAuth(false);
+  };
+
   return (
     <MainTab.Navigator screenOptions={{ tabBarShowLabel: false }}>
       <MainTab.Screen
@@ -24,17 +35,50 @@ const TabRouter = () => {
           tabBarIcon: ({ focused, size, color }) => (
             <MaterialCommunityIcons name="postage-stamp" size={size} color={color} />
           ),
+          title: 'Публікації',
+          headerTitleAlign: 'center',
+          headerStyle: {
+            height: 88,
+            backgroundColor: '#fff',
+          },
+          headerTintColor: '#212121',
+          headerTitleStyle: {
+            fontFamily: 'Roboto-Regular',
+            fontWeight: '500',
+            fontSize: 17,
+          },
+          headerRight: () => <SignOutIcon style={{ marginRight: 10 }} onPress={signOut} />,
         }}
-        name="Публікації"
+        name="Posts"
         component={PostsScreen}
       />
       <MainTab.Screen
-        options={{
+        options={({ navigation }) => ({
           tabBarIcon: ({ focused, size, color }) => (
             <AntDesign name="pluscircleo" size={size} color={color} />
           ),
-        }}
-        name="Створити публікацію"
+          title: 'Створити публікацію',
+          headerTitleAlign: 'center',
+          headerStyle: {
+            height: 88,
+            backgroundColor: '#fff',
+          },
+          headerTintColor: '#212121',
+          headerTitleStyle: {
+            fontFamily: 'Roboto-Regular',
+            fontWeight: '500',
+            fontSize: 17,
+          },
+          headerLeft: () => (
+            <GoBackIcon
+              style={{ marginLeft: 16 }}
+              onPress={() => {
+                navigation.goBack();
+              }}
+            />
+          ),
+        })}
+        name="CreatePosts"
         component={CreatePostsScreen}
       />
       <MainTab.Screen
@@ -44,7 +88,7 @@ const TabRouter = () => {
           ),
           headerShown: false,
         }}
-        name="Профіль"
+        name="Profile"
         component={ProfileScreen}
       />
     </MainTab.Navigator>
@@ -89,9 +133,17 @@ export default function useRoute(isAuth) {
         component={TabRouter}
       />
       <MainStack.Screen
-        options={{
-          headerShown: false,
-        }}
+        options={({ navigation }) => ({
+          headerShown: true,
+          headerLeft: () => (
+            <GoBackIcon
+              style={{ marginLeft: 16 }}
+              onPress={() => {
+                navigation.goBack();
+              }}
+            />
+          ),
+        })}
         name="Comments"
         component={CommentsScreen}
       />
